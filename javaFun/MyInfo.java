31e0b0756e22e3041f7bbbed1c363b0ec01f2e62
esppablo <esppablo@amazon.com>
2018-04-23 10:52:40 -0600

    Initial Java Repo

---
+++ javaFun/MyInfo.java
@@ -0,0 +1,9 @@
+// $ touch MyInfo.java
+// $ javac MyInfo.java
+// $ java MyInfo
+
+public class MyInfo {
+    public static void main(String[] args) {
+        System.out.println("My name is Pablo Jinesta\nI am 30 years old\nMy hometown is San Jose, CR");
+    }
+}
