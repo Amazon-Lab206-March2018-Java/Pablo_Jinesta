64f7844e9482b3943172e0102b0e6fe9d3662cb4
esppablo <esppablo@amazon.com>
2018-05-17 12:44:09 -0600

    Login and registration in progress

---
+++ springProjects/LoginRegistration/src/main/java/com/esppablo/loginregistration/controllers/AppController.java
@@ -0,0 +1,47 @@
+package com.esppablo.loginregistration.controllers;
+
+import javax.servlet.http.HttpSession;
+import javax.validation.Valid;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.esppablo.loginregistration.models.User;
+import com.esppablo.loginregistration.services.UserService;
+
+@Controller
+public class AppController {
+    private UserService userService;
+    
+    public AppController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @RequestMapping("/login")
+    public String login_registration(@Valid @ModelAttribute("new_user") User user) {
+        return "index";
+    }
+    
+    @PostMapping("/login")
+    public String login() {
+    	// code here
+    	return "index";
+    }
+    
+    @PostMapping("/registration")
+    public String register(@Valid @ModelAttribute("new_user") User user, BindingResult result, Model model, HttpSession sessio) {
+    	if(result.hasErrors()) {
+    		return "index";
+    	} else {
+            userService.saveWithUserRole(user);
+            return "redirect:/login";  		
+    	}
+    }
+    
+    
+    
+}
